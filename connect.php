85c87f47605e27bb6fb2bf1e70b5e46f00b04cf8
AKASHCS50 <akcount121@gmail.com>
2021-05-29 23:52:52 +0530

    Added database

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
  <?php
 
-  //  $db = mysqli_connect('localhost', 'id11401863_dbms', 'my000webhost', 'id11401863_mysqlonlineforum') or die('Could not connect to database');
-    $db = mysqli_connect('localhost:3307', 'root', '', 'discussx') or die('Could not connect to database');
+    // $db = mysqli_connect(getenv(''), 'root', '', 'discussx') or die('Could not connect to database');
+    $db = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASSWORD'), getenv('DB_NAME')) or die('Could not connect to database');
 
    ?>
